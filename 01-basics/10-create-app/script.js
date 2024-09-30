d6c1b63586cc7e1953462b282eaf62a957bdc1c2
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-09-30 10:48:20 +0300

    Создано Vue приложение, которое рендерит текущую дату

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,21 @@
-import { defineComponent, createApp } from 'vue'
+import { createApp, defineComponent } from 'vue'
+
+const app = createApp(defineComponent({
+  setup() {
+    const date = new Date().toLocaleDateString(navigator.language, {
+      dateStyle: 'long',
+    })
+
+    return {
+      date,
+    }
+  },
+
+  name: 'App',
+
+  template: /* html */`
+    <div>Сегодня {{ date }}</div>
+  `,
+}))
+
+app.mount('#app')
