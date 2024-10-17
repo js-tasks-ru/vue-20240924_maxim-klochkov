f7690c4248c1daee7e1186fa8f1337769311ea24
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-10-17 10:23:45 +0300

    Решение задачи 'Удаляемые Email-ы'

--- 03-components/30-removable-emails/EmailList.js
+++ 03-components/30-removable-emails/EmailList.js
@@ -15,13 +15,16 @@ export default defineComponent({
     },
   },
 
-  template: `
+  emits: ['remove-by-index'],
+
+  template: /* html */`
     <ul class="emails-list unstyled-list" aria-label="Emails">
       <EmailListItem
         v-for="({ email, isMarked }, index) in emails"
         :key="email"
         :email="email"
         :marked="isMarked"
+        @remove-email-item="$emit('remove-by-index', index)"
       />
     </ul>
   `,
