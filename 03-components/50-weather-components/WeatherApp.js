5e20b20582c9b1c7027b4fd5cbc87cb44382fae3
Maksim Klochkov <maksim.klochkov@rt.ru>
2024-10-23 17:51:20 +0300

    Решение задачи 'Компоненты: погода в Средиземье'

--- 03-components/50-weather-components/WeatherApp.js
+++ 03-components/50-weather-components/WeatherApp.js
@@ -1,10 +1,30 @@
 import { defineComponent } from 'vue'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
+import WeatherAlert from './WeatherAlert.js'
+import WeatherCards from './WeatherCards.js'
+import WeatherCardsItem from './WeatherCardsItem.js'
+import WeatherCardsItemConditions from './WeatherCardsItemConditions.js'
+import WeatherCardsItemHeader from './WeatherCardsItemHeader.js'
+import WeatherDetails from './WeatherDetails.js'
+import WeatherDetailsItem from './WeatherDetailsItem.js'
+import WeatherTitle from './WeatherTitle.js'
+
 import './WeatherApp.css'
 
 export default defineComponent({
   name: 'WeatherApp',
+
+  components: {
+    WeatherAlert,
+    WeatherCards,
+    WeatherCardsItem,
+    WeatherDetails,
+    WeatherDetailsItem,
+    WeatherCardsItemConditions,
+    WeatherCardsItemHeader,
+    WeatherTitle,
+  },
 
   setup() {
     const weatherData = getWeatherData()
@@ -24,57 +44,47 @@ export default defineComponent({
 
   template: /* html */`
     <div>
-      <h1 class="title">Погода в Средиземье</h1>
-
-      <ul class="weather-list unstyled-list">
-        <li
+      <WeatherTitle title="Погода в Средиземье" />
+      <WeatherCards>
+        <WeatherCardsItem
           v-for="weather in weatherData"
           :key="weather.geographic_name"
-          class="weather-card"
-          :class="
-            {
-              'weather-card--night': isNight(weather.current.dt, weather.current.sunrise, weather.current.sunset),
-            }
-          "
+          :is-night="isNight(weather.current.dt, weather.current.sunrise, weather.current.sunset)"
         >
-        <div v-if="weather.alert" class="weather-alert">
-          <span class="weather-alert__icon">⚠️</span>
-          <span class="weather-alert__description">
-            {{ weather.alert.sender_name }} : {{ weather.alert.description }}
-          </span>
-        </div>
-        <div>
-          <h2 class="weather-card__name">{{ weather.geographic_name }}</h2>
-          <div class="weather-card__time">{{ weather.current.dt }}</div>
-        </div>
-        <div class="weather-conditions">
-          <div class="weather-conditions__icon" :title="weather.current.weather.description">
-            {{ weatherIcons[weather.current.weather.id] }}
-          </div>
-          <div class="weather-conditions__temp">
-            {{ (weather.current.temp - 273.15).toFixed(1) }} °C
-          </div>
-        </div>
-        <div class="weather-details">
-          <div class="weather-details__item">
-            <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-            <div class="weather-details__item-value">{{ (weather.current.pressure * 0.75).toFixed(0) }}</div>
-          </div>
-          <div class="weather-details__item">
-            <div class="weather-details__item-label">Влажность, %</div>
-            <div class="weather-details__item-value">{{ weather.current.humidity }}</div>
-          </div>
-          <div class="weather-details__item">
-            <div class="weather-details__item-label">Облачность, %</div>
-            <div class="weather-details__item-value">{{ weather.current.clouds }}</div>
-          </div>
-          <div class="weather-details__item">
-            <div class="weather-details__item-label">Ветер, м/с</div>
-            <div class="weather-details__item-value">{{ weather.current.wind_speed }}</div>
-          </div>
-        </div>
-        </li>
-      </ul>
+          <WeatherAlert
+            v-if="weather.alert"
+            :sender-name="weather.alert.sender_name"
+            :description="weather.alert.description"
+          />
+          <WeatherCardsItemHeader
+            :name="weather.geographic_name"
+            :time="weather.current.dt"
+          />
+          <WeatherCardsItemConditions
+            :title="weather.current.weather.description"
+            :icon-id="weatherIcons[weather.current.weather.id]"
+            :temperature="(weather.current.temp - 273.15).toFixed(1)"
+          />
+          <WeatherDetails>
+            <WeatherDetailsItem
+              label="Давление, мм рт. ст."
+              :value="(weather.current.pressure * 0.75).toFixed(0)"
+            />
+            <WeatherDetailsItem
+              label="Влажность, %"
+              :value="weather.current.humidity"
+            />
+            <WeatherDetailsItem
+              label="Облачность, %"
+              :value="weather.current.clouds"
+            />
+            <WeatherDetailsItem
+              label="Ветер, м/с"
+              :value="weather.current.wind_speed"
+            />
+          </WeatherDetails>
+        </WeatherCardsItem>
+      </WeatherCards>
     </div>
   `,
 })
